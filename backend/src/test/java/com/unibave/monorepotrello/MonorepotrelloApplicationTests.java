59ad4bb28293e6289d444d19b377e4f458e1ef2e
Pietro Gonçalves <pietro.goncalves@zallpy.com>
2021-10-18 19:52:32 -0300

    Ajuste Flyway e Docker Compose

---
+++ backend/src/test/java/com/unibave/monorepotrello/MonorepotrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibave.monorepotrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonorepotrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
