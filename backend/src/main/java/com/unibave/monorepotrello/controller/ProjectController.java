9c7d41b8c06b097b79309546ea71b41d750a0e73
Gustavo-Dorigon-Coan <88582581+Gustavo-Dorigon-Coan@users.noreply.github.com>
2021-11-01 21:10:50 -0300

    Cria endpoints para projects

--- backend/src/main/java/com/unibave/monorepotrello/controller/ProjectController.java
+++ backend/src/main/java/com/unibave/monorepotrello/controller/ProjectController.java
@@ -1,15 +1,14 @@
 package com.unibave.monorepotrello.controller;
 
-
 import com.unibave.monorepotrello.constant.ResourceName;
+import com.unibave.monorepotrello.model.Project;
 import com.unibave.monorepotrello.service.ProjectService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.unibave.monorepotrello.model.Project;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(ResourceName.PROJECTS)
@@ -21,38 +20,17 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    @PostMapping
+    @GetMapping(value = "/user/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<Project>> findByUserId(
+            @PathVariable("id") Long id){
+        return ResponseEntity.ok().body(projectService.findByUserId(id));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody Project project){
         projectService.save(project);
     }
 
-    @GetMapping
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<List<Project>> findAll(){
-        return ResponseEntity.ok().body(projectService.findAll());
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public void delete (@PathVariable("id") long id){
-        projectService.delete(id);
-    }
-
-    @PutMapping({"/id"})
-    @ResponseStatus(HttpStatus.OK)
-    public void update(
-            @PathVariable("id") Long id,
-            @RequestBody Project project){
-        projectService.update(id, project);
-    }
-
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Optional<Project>> findById(
-            @PathVariable("id") Long id){
-        return ResponseEntity.ok().body(projectService.findById(id));
-    }
 }
