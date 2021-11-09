7730c821f201340135bc72c276183b6daddccd0b
Gustavo-Dorigon-Coan <88582581+Gustavo-Dorigon-Coan@users.noreply.github.com>
2021-11-08 21:38:10 -0300

    Adiciona color e order no model ListOfCards

--- backend/src/main/java/com/unibave/monorepotrello/model/ListOfCards.java
+++ backend/src/main/java/com/unibave/monorepotrello/model/ListOfCards.java
@@ -34,6 +34,22 @@ public class ListOfCards {
     @Column(name = "order_sequence")
     private int order;
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
     public Long getId() {
         return id;
     }
